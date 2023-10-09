a3a5a05d2bb1449ad06ad708079978020161868f
JoeK <ohhjassassin@gmail.com>
2023-10-09 14:45:47 +0100

    adding project files

---
+++ TopTrump/Models/Card.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TopTrump.Models
+{
+    public class Card
+    {
+        public int CardId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "Card Name")]
+        [DataType(DataType.Text)]
+        public required string CardName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "Stat 1")]
+        public required int Stat1 { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "Stat 2")]
+        public required int Stat2 { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "Stat 1")]
+        public required int Stat3 { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "Stat 2")]
+        public required int Stat4 { get; set; }
+        [ForeignKey("Deck")]
+        public int DeckId { get; set; }
+
+        [MaxLength(255)]
+        [Display(Name = "Description")]
+        [DataType(DataType.Text)]
+        public string? Description { get; set; }
+    }
+}
---
+++ TopTrump/Models/Deck.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TopTrump.Models
+{
+    public class Deck
+    {
+        public int DeckId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "Deck Name")]
+        [DataType(DataType.Text)]
+        public string? DeckName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "Stat 1")]
+        [DataType(DataType.Text)]
+        public required string Stat1 { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "Stat 2")]
+        [DataType(DataType.Text)]
+        public required string Stat2 { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "Stat 3")]
+        [DataType(DataType.Text)]
+        public required string Stat3 { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "Stat 4")]
+        [DataType(DataType.Text)]
+        public required string Stat4 { get; set; }
+        public List<Card>? Cards { get; set; }
+    }
+}
