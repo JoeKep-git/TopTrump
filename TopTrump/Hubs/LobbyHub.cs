13459aa6022da806cdd4bf45c394049d5360ad9c
JoeK <ohhjassassin@gmail.com>
2023-10-17 13:51:53 +0100

    no longer creating lobby with join lobby text field. TODO: join the lobby session

--- TopTrump/Hubs/LobbyHub.cs
+++ TopTrump/Hubs/LobbyHub.cs
@@ -6,19 +6,17 @@ namespace TopTrump.Hubs
     [Authorize]
     public class LobbyHub : Hub
     {
-        private static List<string> lobbyNames = new List<string>();
+        private static Dictionary<string, List<string>> lobbies = new Dictionary<string, List<string>>();
         public async Task CreateLobby(string lobbyName)
         {
-            // Create a new lobby
-            // You may want to add lobby to a list or store in a database
-            if(!lobbyNames.Contains(lobbyName))
+            if (!lobbies.ContainsKey(lobbyName))
             {
-                lobbyNames.Add(lobbyName);
+                lobbies.Add(lobbyName, new List<string>());
                 await Clients.All.SendAsync("UpdateLobbies", lobbyName);
+                await Clients.Caller.SendAsync("LobbyCreated", lobbyName);
             }
             else
             {
-                // Lobby name already exists, handle this situation (e.g., inform the user)
                 await Clients.Caller.SendAsync("LobbyNameExists", lobbyName);
             }
 
@@ -26,9 +24,16 @@ namespace TopTrump.Hubs
 
         public async Task JoinLobby(string lobbyName)
         {
-            // Add player to the lobby
-            // Notify other players in the lobby
-            await Clients.All.SendAsync("UpdateLobbies", lobbyName);
+            if (lobbies.ContainsKey(lobbyName))
+            {
+                lobbies[lobbyName].Add(Context.User.Identity.Name);
+                await Groups.AddToGroupAsync(Context.ConnectionId, lobbyName);
+                await Clients.Group(lobbyName).SendAsync("PlayerJoined", Context.User.Identity.Name);
+            }
+            else
+            {
+                await Clients.Caller.SendAsync("LobbyNotFound", lobbyName);
+            }
         }
     }
 
